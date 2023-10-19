712193053b9e7569e41f9772bb25b66379c7e978
Jolin Zhang <jolin51502@gmail.com>
2023-10-18 18:10:52 -0700

    ip address change

--- clientTest.py
+++ clientTest.py
@@ -1,6 +1,6 @@
 import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('131.179.74.160', 8080))
+client.connect(('169.232.126.32', 8080))
 client.sendall('I am CLIENT\n'.encode())
 from_server = client.recv(4096)
 client.close()
